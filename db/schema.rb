610459bf2b4208cdbf0c9d3daa5e9b44cb80ee90
jonBarcus <barcus.j@gmail.com>
2018-06-11 13:30:14 -0400

    created a migration that added USER_ID to the CLUBS table, started using PRY_REMOTE instead of PRY because it works better with FOREMAN, also started getting the CLUB html ready and the CRUD working.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_213038) do
+ActiveRecord::Schema.define(version: 2018_06_11_162048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_06_05_213038) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "country_assignments", force: :cascade do |t|
