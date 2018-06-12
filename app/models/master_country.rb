4b2078efd0131c884bc78bd3ccccef12912159a1
jonBarcus <barcus.j@gmail.com>
2018-06-12 10:21:39 -0400

    added ANNOTATE to the gemfile and ran it

--- app/models/master_country.rb
+++ app/models/master_country.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: master_countries
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string
+#  un_member   :boolean
+#  al_member   :boolean
+#  oas_member  :boolean
+#  au_member   :boolean
+#  eu_member   :boolean
+#  nato_member :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class MasterCountry < ActiveRecord::Base
 
 	belongs_to :country_assignment
